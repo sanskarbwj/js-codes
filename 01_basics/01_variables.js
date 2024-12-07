966caa1e875d40e64417727d8984a20a142d2a89
sanskarbwj <153425852+sanskarbwj@users.noreply.github.com>
2024-12-07 17:22:07 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const a = "sanskar"                     //constant
+var b = 4                               //dont use
+let c = "mmm chaddi nahi pahne ho na"   //variable
+d = "aaj konse color ki chaddi pahne ho"//variable
+let f
+console.table([a,b,c,d,"e"]) 
+console.log(f)
