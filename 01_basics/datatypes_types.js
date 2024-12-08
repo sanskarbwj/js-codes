7119c8b143f85707154a962ab62f30a599b15be0
sanskarbwj <153425852+sanskarbwj@users.noreply.github.com>
2024-12-08 03:56:09 +0000

    container config done

---
+++ 01_basics/datatypes_types.js
@@ -0,0 +1,30 @@
+/*
+
+
+->there are two types of data types
+
+  1.primitive
+  2.nonprimitive
+
+  primitive(7 types) : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+  reference(non primitive) : Array, Objects, Functions
+
+
+*/
+
+//aray
+const heros = ["spiderman","captian america","batman"]
+//object
+let obj1={
+    name : "rajar dalal",
+    age : 29,
+    work : "power lifter"
+}
+//function
+const trea_baap =  function(){
+console.log("teri ma ki chut")
+}
+console.log(trea_baap)
+console.log(obj1)
+console.log(heros)
